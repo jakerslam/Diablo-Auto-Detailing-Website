f88c3746e74a9c6cdb92bceca4220843095f7f05
Jay Haslam <jakerslam@gmail.com>
2026-05-18 15:28:38 -0600

    Fix custom domain base path for Pages builds

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 const repository = process.env.GITHUB_REPOSITORY || '';
 const repositoryName = repository.includes('/') ? repository.split('/')[1] : '';
 const isGitHubPages = process.env.GITHUB_PAGES === 'true' && repositoryName;
-const basePath = isGitHubPages ? `/${repositoryName}` : '';
+const configuredSiteUrl = process.env.PUBLIC_SITE_URL || '';
+const usesCustomDomain = /https?:\/\/(www\.)?diabloautodetailing\.com\/?$/i.test(configuredSiteUrl);
+const basePath = isGitHubPages && !usesCustomDomain ? `/${repositoryName}` : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
